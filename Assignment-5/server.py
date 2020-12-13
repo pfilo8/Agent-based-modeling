2fc0c1f9fd034046c6cf5d37ffbb7cfde4e6102d
Anna Szymanek <szymanek_a@wp.pl>
2020-12-13 15:47:31 +0100

    Create server.py

---
+++ Assignment-5/server.py
@@ -0,0 +1,28 @@
+import random
+
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid, ChartModule, TextElement
+from mesa.visualization.UserParam import UserSettableParameter
+
+from mesa.visualization.TextVisualization import (TextData, TextGrid, TextVisualization)
+
+from model.NagelSchreckenbergModel import CarModel
+
+def car_draw(agent):
+
+    r = lambda: random.randint(0,255)
+    color = '#%02X%02X%02X' % (r(),r(),r())
+    return {"Shape": "rect", "w": 0.5,"h": 40, "Filled": "true", "Layer": 0, "Color": color, "stroke_color": "#00FF00" }
+
+canvas_element = CanvasGrid(car_draw, 100, 100, 500, 500)
+
+model_params = {
+    "height": 100,
+    "width": 100,
+    "dawdle_prob": UserSettableParameter("slider", "Dawdle Probability", 0.1, 0, 1, 0.05),
+    "car_amount": UserSettableParameter("slider", "Number of Cars", 10, 1, 100 , 1),
+}
+
+server = ModularServer(CarModel,
+                       [canvas_element],
+                       "Nagel-Schreckenberg", model_params)
